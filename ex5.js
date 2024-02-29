d99d8904817e931e1fe26b5ab29151e980bea315
Asma Shahid <asmashahid795@gmail.com>
2024-02-29 19:14:02 +0500

    Add files via upload
    I repeated ex4 but this time i use famous_person to replace author

---
+++ ex5.js
@@ -0,0 +1,3 @@
+var famous_Person = "Thomas A. Edison";
+var message = "I have not failed. I've just found 10,000 ways that won't work.";
+console.log("Once ".concat(famous_Person, " said,\"").concat(message, "\""));
---
+++ ex5.ts
@@ -0,0 +1,3 @@
+let famous_Person: string = "Thomas A. Edison";
+let message: string = "I have not failed. I've just found 10,000 ways that won't work.";
+console.log(`Once ${famous_Person} said,"${message}"`);
